6670fbdae64e82ed7c2b7aff0752b75c44f8d230
bluepill5 <alexandro.mayoral@gmail.com>
2022-05-08 18:22:53 -0500

    Vue CLI

--- Work 02/src/app.js
+++ Work 02/src/app.js
@@ -1,6 +1,66 @@
+Vue.component('vue-table', {
+    props: {
+        titulos: {
+            type: Array,
+            required: true
+        },
+        registros: {
+            type: Array
+        }
+    },
+    template: `
+        <div>
+            <table class="table table-striped table-dark">
+                <thead class="thead-light">
+                    <tr>
+                        <th v-for="(item, i) in titulos" :key="i" scope="col">{{ item }}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(registro, i) in registros" key="i">
+                        <th>{{ i + 1 }}</th>
+                        <th>{{ registro.nombre }}</th>
+                        <th>{{ registro.apellido }}</th>
+                        <th>{{ registro.provincia }}</th>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    `
+})
 
-
-
+const app = new Vue({
+    el: '#app',
+    data : {
+        tabla1: {
+            titulos: ['ID', 'Nombre', 'Apellidos', 'Provincia'],
+            registros: [
+                {
+                    nombre: 'Jose',
+                    apellido: 'Velez',
+                    provincia: 'San Luis'
+                },
+                {
+                    nombre: 'Ramon',
+                    apellido: 'Gomez',
+                    provincia: 'Santa Fe'
+                },
+                {
+                    nombre: 'Marcelo',
+                    apellido: 'Godino',
+                    provincia: 'Buenos Aires'
+                },
+                {
+                    nombre: 'Ramiro',
+                    apellido: 'Gimenez',
+                    provincia: 'Salta'
+                },
+            ]
+        },
+        tabla2: {},
+        tabla3: {}
+    }
+});
 
 
 
