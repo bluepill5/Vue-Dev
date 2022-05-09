d5fa2f0311d94a229955aef3d982c4279630941f
bluepill5 <alexandro.mayoral@gmail.com>
2022-05-08 19:35:34 -0500

    custom component

--- Work 02/src/app.js
+++ Work 02/src/app.js
@@ -6,19 +6,25 @@ Vue.component('vue-table', {
         },
         registros: {
             type: Array
-        }
+        },
+        estiloHead: {
+            type: String
+        },
+        estiloBody: {
+            type: String
+        },
     },
     template: `
         <div>
-            <table class="table table-striped table-dark">
+            <table :class="estiloBody">
                 <thead class="thead-light">
                     <tr>
                         <th v-for="(item, i) in titulos" :key="i" scope="col">{{ item }}</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr v-for="(registro, i) in registros" key="i">
-                        <th>{{ i + 1 }}</th>
+                    <tr v-for="(registro, j) in registros" :key="j">
+                        <th>{{ j + 1 }}</th>
                         <th>{{ registro.nombre }}</th>
                         <th>{{ registro.apellido }}</th>
                         <th>{{ registro.provincia }}</th>
@@ -55,10 +61,64 @@ const app = new Vue({
                     apellido: 'Gimenez',
                     provincia: 'Salta'
                 },
-            ]
+            ],
+            estiloBody: "table table-striped table-dark",
+            estiloHead: "table-dark",
         },
-        tabla2: {},
-        tabla3: {}
+        tabla2: {
+            titulos: ['ID', 'Nombre', 'Apellidos', 'Provincia'],
+            registros: [
+                {
+                    nombre: 'Jose',
+                    apellido: 'Velez',
+                    provincia: 'San Luis'
+                },
+                {
+                    nombre: 'Ramon',
+                    apellido: 'Gomez',
+                    provincia: 'Santa Fe'
+                },
+                {
+                    nombre: 'Marcelo',
+                    apellido: 'Godino',
+                    provincia: 'Buenos Aires'
+                },
+                {
+                    nombre: 'Ramiro',
+                    apellido: 'Gimenez',
+                    provincia: 'Salta'
+                },
+            ],
+            estiloBody: "table table-striped table-warning",
+            estiloHead: "table-dark",
+        },
+        tabla3: {
+            titulos: ['ID', 'Nombre', 'Apellidos', 'Provincia'],
+            registros: [
+                {
+                    nombre: 'Jose',
+                    apellido: 'Velez',
+                    provincia: 'San Luis'
+                },
+                {
+                    nombre: 'Ramon',
+                    apellido: 'Gomez',
+                    provincia: 'Santa Fe'
+                },
+                {
+                    nombre: 'Marcelo',
+                    apellido: 'Godino',
+                    provincia: 'Buenos Aires'
+                },
+                {
+                    nombre: 'Ramiro',
+                    apellido: 'Gimenez',
+                    provincia: 'Salta'
+                },
+            ],
+            estiloBody: "table table-striped table-info",
+            estiloHead: "table-dark",
+        }
     }
 });
 
